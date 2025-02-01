c510032a24e24de9f11ffa493b53d3946c3df894
Ahmad Shahzad <shzdehmd@gmail.com>
2025-02-01 09:40:11 +0500

    Created a simple hello-world express app.

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('welcome to noteshare.');
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '127.0.0.1';
+
+app.get('/', (req, res) => {
+    res.send('Hello, noteshare!');
+});
+
+app.listen(PORT, () => {
+    console.log(`noteshare is running on http://${HOST}:${PORT}`);
+});
